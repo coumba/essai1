5f863d16789c3801601e79e8933fb59a1e6002e3
coumba <dcoumbakebe@yahoo.fr>
2013-01-19 11:18:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Essai1::Application.config.secret_token = '3840328d5b21a92e855296ae17e976c6e014eb57fabed65c0709f80a525f64dda080aa54dff819747c69052fd0c73568bc575f54ffcabb4677c3744a6ab553a0'
